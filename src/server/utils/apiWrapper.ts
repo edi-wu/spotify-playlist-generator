9fe41678e3f0dad53ca90551e8eae244f18853e8
Eddie Wu <eddie.wu.info@gmail.com>
2022-12-09 11:50:04 -0800

    Instantiate Spotify API wrapper

---
+++ src/server/utils/apiWrapper.ts
@@ -0,0 +1,12 @@
+import SpotifyWebApi from 'spotify-web-api-node';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI,
+});
+
+export default spotifyApi;
