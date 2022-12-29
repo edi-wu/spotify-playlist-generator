8123297e06c7e3bf58d1a8c9ce5c2e2274516306
Eddie Wu <eddie.wu.info@gmail.com>
2022-12-29 12:09:53 -0800

    Extract function to start playlist playback

--- src/client/__tests__/services/playlistService.test.ts
+++ src/client/__tests__/services/playlistService.test.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import generatePlaylist from '../../services/playlistService';
+import { generatePlaylist, playPlaylist } from '../../services/playlistService';
 import { ENDPOINTS } from '../../constants';
 import { FormData } from '../../types';
 
@@ -45,5 +45,38 @@ describe('testing playlist generation handler', () => {
     expect(mockedAxios.post).toHaveBeenNthCalledWith(1, url, input);
     expect(response instanceof Error).toBe(true);
     expect(response).toMatchObject(expectedError);
+  });
+});
+
+describe('testing function to start playing selected playlist', () => {
+  const mockDeviceId = 'my-device-123';
+  const mockPlaylistUri = 'playlist-123';
+  const mockToken = 'access-token';
+
+  test('function should make put request with correct arguments', async () => {
+    await playPlaylist(mockDeviceId, mockPlaylistUri, mockToken);
+    expect(mockedAxios.put.mock.calls.length).toBe(1);
+    expect(mockedAxios.put.mock.calls[0][0]).toMatch(mockDeviceId);
+    expect(mockedAxios.put.mock.calls[0][1]).toEqual(
+      expect.objectContaining({ context_uri: expect.stringContaining(mockPlaylistUri) })
+    );
+    expect(mockedAxios.put.mock.calls[0][2]).toEqual(
+      expect.objectContaining({
+        headers: expect.objectContaining({ Authorization: expect.stringContaining(mockToken) }),
+      })
+    );
+  });
+
+  test('function should console log error message', async () => {
+    const logSpy = jest.spyOn(console, 'log');
+    mockedAxios.put.mockRejectedValueOnce(new Error('playlist playback error'));
+    await playPlaylist(mockDeviceId, mockPlaylistUri, mockToken);
+    expect(logSpy.mock.calls.length).toBe(1);
+    expect(logSpy.mock.calls[0][1]).toBe('playlist playback error');
+
+    mockedAxios.put.mockRejectedValueOnce(-1);
+    await playPlaylist(mockDeviceId, mockPlaylistUri, mockToken);
+    expect(logSpy.mock.calls.length).toBe(2);
+    expect(logSpy.mock.calls[1][1]).toBe(String(-1));
   });
 });
