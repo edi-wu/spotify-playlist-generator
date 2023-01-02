124a6ff73cfe418c9e75e1f8f0b137ab82fd44be
Eddie Wu <eddie.wu.info@gmail.com>
2023-01-02 13:06:46 -0800

    Remove test for direct route access

--- src/client/__tests__/app.test.tsx
+++ src/client/__tests__/app.test.tsx
@@ -5,8 +5,8 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom';
-import { createMemoryRouter, RouterProvider } from 'react-router-dom';
-import App, { routesConfig } from '../App';
+// import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import App from '../App';
 
 describe('initial app rendering and navigation', () => {
   test('app should render homepage at start', () => {
@@ -14,12 +14,5 @@ describe('initial app rendering and navigation', () => {
     expect(screen.getByText(/axolotl beats/i)).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /login with spotify/i })).toBeInTheDocument();
   });
-  // TODO: test below should be revised to ensure that access to routes is blocked if not auth'ed
-  test('direct access to available route should render', () => {
-    const router = createMemoryRouter(routesConfig, {
-      initialEntries: ['/form'],
-    });
-    render(<RouterProvider router={router} />);
-    expect(screen.getByText(/user input form/i)).toBeInTheDocument();
-  });
+  // TODO: test that direct access to routes is blocked if not auth'ed
 });
