afae7e6827b51c46cfa48c78dfe5f06fd0e73ec7
Eddie Wu <eddie.wu.info@gmail.com>
2023-01-18 13:32:12 -0800

    Move font import from globalStyles to index.html

--- src/client/globalStyles.ts
+++ src/client/globalStyles.ts
@@ -2,7 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
-    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
     margin: 0;
     padding: 0;
     font-family: Roboto, Helvetica, Sans-Serif;
